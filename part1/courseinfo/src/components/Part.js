b593ab081db0bdb182855bcbf80b51cd6e921e7d
Mario Alzamora <marioalejandroalzamora@gmail.com>
2022-07-29 18:13:01 -0300

    Part1

---
+++ part1/courseinfo/src/components/Part.js
@@ -0,0 +1,10 @@
+const Part = (props) => {
+    return(
+        <>
+            <p>{props.part} {props.exercises}</p>
+        </>
+    )
+
+}
+
+export default Part
---
+++ part1/courseinfo/src/components/Total.js
@@ -0,0 +1,7 @@
+const Total = (props) => {
+    return (
+        <p>Total Number of exercises {props.total}</p>
+    )
+}
+
+export default Total
